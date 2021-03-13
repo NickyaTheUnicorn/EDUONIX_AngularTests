5cc202476fb9e2cbf33eda3ff8a396bdc0adc40a
yannick.renner <yannick.renner@he-arc.ch>
2021-03-13 15:12:51 +0100

    Dashboard tested

--- src/app/dashboard/dashboard.component.spec.ts
+++ src/app/dashboard/dashboard.component.spec.ts
@@ -1,16 +1,30 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { UserInterface } from '../user-interface';
 
 import { DashboardComponent } from './dashboard.component';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
+  let userTest: UserInterface;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ DashboardComponent ]
     })
     .compileComponents();
+
+    userTest = {
+      id: 1,
+      firstName: 'firstName',
+      lastName: 'lastName',
+      email: 'email',
+      password: 'password'
+    };
+
+    localStorage.clear();
+    localStorage.setItem('users-data', JSON.stringify([userTest]));
+    localStorage.setItem('currentUser', JSON.stringify(userTest.id));
   });
 
   beforeEach(() => {
@@ -20,7 +34,15 @@ describe('DashboardComponent', () => {
   });
 
   it('should create', () => {
-    localStorage.setItem('currentUser', JSON.stringify(1));
     expect(component).toBeTruthy();
   });
+
+  it('should have a user', () => {
+    expect(component.currentUser).not.toBeNull();
+  });
+
+  it('should have the correct user\'s first name', () => {
+    expect(component.currentUser.firstName).toEqual(userTest.firstName);
+  });
+
 });

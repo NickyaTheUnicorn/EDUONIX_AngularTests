2409e69932744ad159acbb735f9b99c6644fd520
yannick.renner <yannick.renner@he-arc.ch>
2021-03-13 16:23:24 +0100

    form button and form field components tested

--- src/app/form-button/form-button.component.spec.ts
+++ src/app/form-button/form-button.component.spec.ts
@@ -1,10 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ButtonConfig } from '../field-interface';
 
 import { FormButtonComponent } from './form-button.component';
 
 describe('FormButtonComponent', () => {
   let component: FormButtonComponent;
   let fixture: ComponentFixture<FormButtonComponent>;
+  let buttonTest: ButtonConfig;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -14,12 +16,27 @@ describe('FormButtonComponent', () => {
   });
 
   beforeEach(() => {
+    buttonTest = {
+      type: 'button',
+      value: 'value',
+      class: 'btn btn-primary'
+    };
+
     fixture = TestBed.createComponent(FormButtonComponent);
     component = fixture.componentInstance;
+    component.button = buttonTest;
     fixture.detectChanges();
-  });
+    });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have correct type', () => {
+    expect(component.button.type).toEqual(buttonTest.type);
+  });
+
+  it('should have correct value', () => {
+    expect(component.button.value).toEqual(buttonTest.value);
   });
 });
--- src/app/form-field/form-field.component.spec.ts
+++ src/app/form-field/form-field.component.spec.ts
@@ -1,10 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FieldConfig, Validators } from '../field-interface';
 
 import { FormFieldComponent } from './form-field.component';
 
 describe('FormFieldComponent', () => {
   let component: FormFieldComponent;
   let fixture: ComponentFixture<FormFieldComponent>;
+  let formFieldTestComponent: FieldConfig;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -14,12 +16,83 @@ describe('FormFieldComponent', () => {
   });
 
   beforeEach(() => {
+    formFieldTestComponent = {
+      inputType: 'email',
+      label: 'label',
+      name: 'fieldName',
+      validated: false,
+      validator: [
+        {
+          message: 'Validator Message'
+        },
+      ],
+    };
     fixture = TestBed.createComponent(FormFieldComponent);
     component = fixture.componentInstance;
+    component.field = formFieldTestComponent;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should be not validated email', () => {
+    formFieldTestComponent.value = 'test';
+    component.onBlur(formFieldTestComponent.value);
+    fixture.detectChanges();
+    expect(component.field.validated).toBeFalse();
+  });
+
+  it('should be validated email', () => {
+    formFieldTestComponent.value = 'test@test.com';
+    component.onBlur(formFieldTestComponent.value);
+    fixture.detectChanges();
+    expect(component.field.validated).toBeTrue();
+  });
+
+  it('should be not validated min', () => {
+    formFieldTestComponent.inputType = 'text';
+    formFieldTestComponent.validator = [
+      {
+        min: 3,
+        max: 5,
+        message: 'Message'
+      }
+    ];
+    formFieldTestComponent.value = '12';
+    component.onBlur(formFieldTestComponent.value);
+    fixture.detectChanges();
+    expect(component.field.validated).toBeFalse();
+  });
+
+  it('should be not validated on max', () => {
+    formFieldTestComponent.inputType = 'text';
+    formFieldTestComponent.validator = [
+      {
+        min: 3,
+        max: 5,
+        message: 'Message'
+      }
+    ];
+    formFieldTestComponent.value = '123456';
+    component.onBlur(formFieldTestComponent.value);
+    fixture.detectChanges();
+    expect(component.field.validated).toBeFalse();
+  });
+
+  it('should be validated min-max', () => {
+    formFieldTestComponent.inputType = 'text';
+    formFieldTestComponent.validator = [
+      {
+        min: 3,
+        max: 5,
+        message: 'Message'
+      }
+    ];
+    formFieldTestComponent.value = '1234';
+    component.onBlur(formFieldTestComponent.value);
+    fixture.detectChanges();
+    expect(component.field.validated).toBeTrue();
   });
 });
